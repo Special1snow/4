4ebba2cb8869316a62c90585073be1ceef14fa4b
Special1snow <special1snow@sk.com>
2024-10-15 14:14:39 +0900

    Update streamlit-job-skill-comparison.py

--- streamlit-job-skill-comparison.py
+++ streamlit-job-skill-comparison.py
@@ -207,7 +207,6 @@ if page == "역량 입력 및 비교":
                 theta=user_skills.index,
                 fill='toself',
                 name='Your Skills',
-                color='red'
             ))
         
             fig.add_trace(go.Scatterpolar(
